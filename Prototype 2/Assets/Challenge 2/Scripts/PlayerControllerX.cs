63a0faeb5126e0a445df7918a57bb284325812ea
ShubrajSalgotra <salgotrajshub56@gmail.com>
2024-02-21 21:44:08 +0530

    First release

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float timerFloat = 1.0f;
+    private float spawnWaitTime = 2.0f;
+
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timerFloat += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timerFloat > spawnWaitTime)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timerFloat = 0;
+        }
+
+        
+    }
+}
