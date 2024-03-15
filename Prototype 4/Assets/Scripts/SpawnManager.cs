32b5a7296a49a6fc3da3f11ef493a816fa907ab2
ShubrajSalgotra <salgotrajshub56@gmail.com>
2024-03-15 17:33:20 +0530

    completed prototype 4

---
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9.0f;
+    public int enemycount;
+    public int waveNumber = 1;
+    public GameObject powerupPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //float spawnPosX  = Random.Range(-spawnRange, spawnRange);
+        // float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        //Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        //Instantiate(enemyPrefab, randomPos, enemyPrefab.transform.rotation);
+
+        //Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+
+        spawnEnemyWave(waveNumber);
+        Instantiate(powerupPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemycount = FindObjectsOfType<Enemy>().Length;
+        if (enemycount == 0)
+        {
+            waveNumber++;
+            spawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+
+    void spawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+        
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+
+        return randomPos;
+    }
+}
